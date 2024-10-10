11bab0cd8858e5f199338e095ac8527effbe4e32
DURUSHETTI HARSHITH KUMAR <117809278+harshith-durushetti@users.noreply.github.com>
2024-10-10 21:00:08 +0530

    Revert Update IndexController.java

--- SpringBootMVCExample/SpringBootMVCExample/src/main/java/com/example/demo/controller/IndexController.java
+++ SpringBootMVCExample/SpringBootMVCExample/src/main/java/com/example/demo/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/details")
+
 public class IndexController implements ErrorController{
 
     private static final String PATH = "/error";
